e5ace9148889606ab1c5ec3107f45935eb7b19ae
yashas8197 <yashasjr7@gmail.com>
2025-11-30 20:35:48 +0530

    chore(backend): update requirements and adjust Django settings

--- backend/teamshiksha/teamshiksha/settings.py
+++ backend/teamshiksha/teamshiksha/settings.py
@@ -91,10 +91,15 @@ WSGI_APPLICATION = 'teamshiksha.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'ziDknEYoCBDMEeoLuZhrTyUlhTofuxes',
+        'HOST': 'crossover.proxy.rlwy.net',
+        'PORT': '35723',
     }
 }
 
